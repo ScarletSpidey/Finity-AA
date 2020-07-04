595c9c1d266d999aea7e8bf8f92283699631468a
Shobit Puri <shobitpuri2@gmail.com>
2020-07-04 22:39:33 +0530

    Installed sass,bootstrap

---
+++ finity-ui/src/Containers/App.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import LoginPage from '../components/LoginPage/LoginPage'
+import './App.css';
+
+class App extends Component{
+  render(){
+  return (
+        <React.Fragment>
+          <LoginPage/>
+        </React.Fragment>
+  );
+}
+}
+
+export default App;
---
+++ finity-ui/src/components/LoginPage/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+        <React.Fragment>
+            <div className="text-primary">
+                Login Page
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default LoginPage;
