ccdb774ab5832de81c02ae32c4a4865fe6e33080
Shobit Puri <shobitpuri2@gmail.com>
2020-07-10 03:10:41 +0530

    Minor Changes

--- client/src/components/ui/RegisterCard/RegisterCard.js
+++ client/src/components/ui/RegisterCard/RegisterCard.js
@@ -49,32 +49,20 @@ const RegisterCard = () => {
             />
           </div>
           <div className='form-row'>
-            <div className='form-group col-md-6'>
-              <label htmlFor='inputCity'>City</label>
+            <div className='form-group col-md-5'>
+              <label htmlFor='inputCity'>City/Town/Village</label>
               <input type='text' className='form-control' id='inputCity' />
             </div>
-            <div className='form-group col-md-4'>
+            <div className='form-group col-md-3'>
               <label htmlFor='inputState'>State</label>
               <select id='inputState' className='form-control'>
                 <option selected>Choose...</option>
                 <option>...</option>
               </select>
             </div>
-            <div className='form-group col-md-2'>
-              <label htmlFor='inputZip'>Zip</label>
+            <div className='form-group col-md-4'>
+              <label htmlFor='inputZip'>Postal Code</label>
               <input type='text' className='form-control' id='inputZip' />
-            </div>
-          </div>
-          <div className='form-group'>
-            <div className='form-check'>
-              <input
-                className='form-check-input'
-                type='checkbox'
-                id='gridCheck'
-              />
-              <label className='form-check-label' htmlFor='gridCheck'>
-                Check me out
-              </label>
             </div>
           </div>
           <button type='submit' className='btn btn-primary'>
